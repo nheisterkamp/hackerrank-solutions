341cd39f7c29768a45b051cb81ef03ba6c500d41
Niels Heisterkamp <niels@2gears.com>
2014-11-20 11:54:52 +0100

    Fix Compute the Average according to issue 1

--- bash/compute-the-average.sh
+++ bash/compute-the-average.sh
@@ -3,8 +3,8 @@ read LINES
 TOTAL=0
 
 for I in $(seq $LINES); do
-	read LINE
-	TOTAL=$(($TOTAL + $LINE))
+    read LINE
+    TOTAL=$(($TOTAL + $LINE))
 done
 
-echo "scale=3; $TOTAL / $LINES" | bc -l
+printf "%.3f\n" `echo $TOTAL / $LINES | bc -l`
